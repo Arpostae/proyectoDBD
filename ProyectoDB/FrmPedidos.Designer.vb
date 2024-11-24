bb58cab4d8e0fdaba6d6e314fbd22f6f3d98178f
Diego Pérez <a365341@uach.mx>
2024-11-24 09:18:01 -0600

    Se agrega vista de frmPedidos

--- ProyectoDB/FrmPedidos.Designer.vb
+++ ProyectoDB/FrmPedidos.Designer.vb
@@ -22,39 +22,40 @@ Partial Class FrmPedidos
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmPedidos))
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
-        Me.FlowLayoutPanel2 = New System.Windows.Forms.FlowLayoutPanel()
-        Me.panelEntradas = New System.Windows.Forms.Panel()
-        Me.Panel2 = New System.Windows.Forms.Panel()
         Me.btnPostres = New System.Windows.Forms.Button()
         Me.btnBebidas = New System.Windows.Forms.Button()
         Me.btnPlatillos = New System.Windows.Forms.Button()
         Me.btnEntradas = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.TabControl1 = New System.Windows.Forms.TabControl()
-        Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.TabControl2 = New System.Windows.Forms.TabControl()
-        Me.TabPage3 = New System.Windows.Forms.TabPage()
-        Me.TabPage4 = New System.Windows.Forms.TabPage()
-        Me.TabPage5 = New System.Windows.Forms.TabPage()
-        Me.TabPage6 = New System.Windows.Forms.TabPage()
-        Me.TabPage7 = New System.Windows.Forms.TabPage()
-        Me.TabPage8 = New System.Windows.Forms.TabPage()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.dgvPedidos = New System.Windows.Forms.DataGridView()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnFinalizar = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.lblTrabajar = New System.Windows.Forms.Label()
+        Me.dgvMostrarComida = New System.Windows.Forms.DataGridView()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtPrecioTotal = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Agregar = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Panel1.SuspendLayout()
-        Me.FlowLayoutPanel1.SuspendLayout()
-        Me.FlowLayoutPanel2.SuspendLayout()
-        Me.panelEntradas.SuspendLayout()
-        Me.TabControl1.SuspendLayout()
-        Me.TabControl2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        CType(Me.dgvPedidos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
+        CType(Me.dgvMostrarComida, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Agregar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Panel1
         '
         Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
-        Me.Panel1.Controls.Add(Me.FlowLayoutPanel1)
         Me.Panel1.Controls.Add(Me.btnPostres)
         Me.Panel1.Controls.Add(Me.btnBebidas)
         Me.Panel1.Controls.Add(Me.btnPlatillos)
@@ -65,39 +66,6 @@ Partial Class FrmPedidos
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(271, 542)
         Me.Panel1.TabIndex = 0
-        '
-        'FlowLayoutPanel1
-        '
-        Me.FlowLayoutPanel1.Controls.Add(Me.FlowLayoutPanel2)
-        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(277, 3)
-        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
-        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(724, 528)
-        Me.FlowLayoutPanel1.TabIndex = 1
-        '
-        'FlowLayoutPanel2
-        '
-        Me.FlowLayoutPanel2.Controls.Add(Me.panelEntradas)
-        Me.FlowLayoutPanel2.Controls.Add(Me.Panel2)
-        Me.FlowLayoutPanel2.Location = New System.Drawing.Point(3, 3)
-        Me.FlowLayoutPanel2.Name = "FlowLayoutPanel2"
-        Me.FlowLayoutPanel2.Size = New System.Drawing.Size(724, 528)
-        Me.FlowLayoutPanel2.TabIndex = 2
-        '
-        'panelEntradas
-        '
-        Me.panelEntradas.Controls.Add(Me.TabControl2)
-        Me.panelEntradas.Controls.Add(Me.TabControl1)
-        Me.panelEntradas.Location = New System.Drawing.Point(3, 3)
-        Me.panelEntradas.Name = "panelEntradas"
-        Me.panelEntradas.Size = New System.Drawing.Size(691, 509)
-        Me.panelEntradas.TabIndex = 1
-        '
-        'Panel2
-        '
-        Me.Panel2.Location = New System.Drawing.Point(3, 518)
-        Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(458, 400)
-        Me.Panel2.TabIndex = 0
         '
         'btnPostres
         '
@@ -121,8 +89,7 @@ Partial Class FrmPedidos
         '
         'btnPlatillos
         '
-        Me.btnPlatillos.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnPlatillos.Font = New System.Drawing.Font("Lucida Bright", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPlatillos.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnPlatillos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
         Me.btnPlatillos.Location = New System.Drawing.Point(17, 233)
         Me.btnPlatillos.Name = "btnPlatillos"
@@ -164,125 +131,173 @@ Partial Class FrmPedidos
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "THAI "
         '
-        'TabControl1
+        'Panel3
         '
-        Me.TabControl1.Controls.Add(Me.TabPage1)
-        Me.TabControl1.Controls.Add(Me.TabPage2)
-        Me.TabControl1.Location = New System.Drawing.Point(333, 57)
-        Me.TabControl1.Name = "TabControl1"
-        Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(667, 271)
-        Me.TabControl1.TabIndex = 1
+        Me.Panel3.Controls.Add(Me.txtPrecioTotal)
+        Me.Panel3.Controls.Add(Me.Label4)
+        Me.Panel3.Controls.Add(Me.Label3)
+        Me.Panel3.Controls.Add(Me.dgvPedidos)
+        Me.Panel3.Location = New System.Drawing.Point(984, 28)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(509, 489)
+        Me.Panel3.TabIndex = 1
         '
-        'TabPage1
+        'dgvPedidos
         '
-        Me.TabPage1.Location = New System.Drawing.Point(4, 25)
-        Me.TabPage1.Name = "TabPage1"
-        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(192, 71)
-        Me.TabPage1.TabIndex = 0
-        Me.TabPage1.Text = "TabPage1"
-        Me.TabPage1.UseVisualStyleBackColor = True
+        Me.dgvPedidos.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgvPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvPedidos.Location = New System.Drawing.Point(3, 37)
+        Me.dgvPedidos.Name = "dgvPedidos"
+        Me.dgvPedidos.RowHeadersWidth = 51
+        Me.dgvPedidos.RowTemplate.Height = 24
+        Me.dgvPedidos.Size = New System.Drawing.Size(503, 377)
+        Me.dgvPedidos.TabIndex = 0
         '
-        'TabPage2
+        'Label3
         '
-        Me.TabPage2.Location = New System.Drawing.Point(4, 25)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(659, 242)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "TabPage2"
-        Me.TabPage2.UseVisualStyleBackColor = True
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(235, 14)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 16)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Pedido"
         '
-        'TabControl2
+        'btnEliminar
         '
-        Me.TabControl2.Controls.Add(Me.TabPage3)
-        Me.TabControl2.Controls.Add(Me.TabPage4)
-        Me.TabControl2.Controls.Add(Me.TabPage5)
-        Me.TabControl2.Controls.Add(Me.TabPage6)
-        Me.TabControl2.Controls.Add(Me.TabPage7)
-        Me.TabControl2.Controls.Add(Me.TabPage8)
-        Me.TabControl2.Location = New System.Drawing.Point(66, 105)
-        Me.TabControl2.Name = "TabControl2"
-        Me.TabControl2.SelectedIndex = 0
-        Me.TabControl2.Size = New System.Drawing.Size(200, 100)
-        Me.TabControl2.TabIndex = 2
+        Me.btnEliminar.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnEliminar.Location = New System.Drawing.Point(831, 205)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(177, 23)
+        Me.btnEliminar.TabIndex = 3
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
         '
-        'TabPage3
+        'btnFinalizar
         '
-        Me.TabPage3.Location = New System.Drawing.Point(4, 25)
-        Me.TabPage3.Name = "TabPage3"
-        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage3.Size = New System.Drawing.Size(192, 71)
-        Me.TabPage3.TabIndex = 0
-        Me.TabPage3.Text = "TabPage3"
-        Me.TabPage3.UseVisualStyleBackColor = True
+        Me.btnFinalizar.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnFinalizar.Location = New System.Drawing.Point(831, 259)
+        Me.btnFinalizar.Name = "btnFinalizar"
+        Me.btnFinalizar.Size = New System.Drawing.Size(177, 23)
+        Me.btnFinalizar.TabIndex = 4
+        Me.btnFinalizar.Text = "Finalizar Compra"
+        Me.btnFinalizar.UseVisualStyleBackColor = True
         '
-        'TabPage4
+        'Panel2
         '
-        Me.TabPage4.Location = New System.Drawing.Point(4, 25)
-        Me.TabPage4.Name = "TabPage4"
-        Me.TabPage4.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage4.Size = New System.Drawing.Size(192, 71)
-        Me.TabPage4.TabIndex = 1
-        Me.TabPage4.Text = "TabPage4"
-        Me.TabPage4.UseVisualStyleBackColor = True
+        Me.Panel2.Controls.Add(Me.PictureBox1)
+        Me.Panel2.Controls.Add(Me.Agregar)
+        Me.Panel2.Controls.Add(Me.TextBox1)
+        Me.Panel2.Controls.Add(Me.Label5)
+        Me.Panel2.Controls.Add(Me.lblTrabajar)
+        Me.Panel2.Controls.Add(Me.dgvMostrarComida)
+        Me.Panel2.Location = New System.Drawing.Point(344, 28)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(509, 489)
+        Me.Panel2.TabIndex = 5
         '
-        'TabPage5
+        'lblTrabajar
         '
-        Me.TabPage5.Location = New System.Drawing.Point(4, 25)
-        Me.TabPage5.Name = "TabPage5"
-        Me.TabPage5.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage5.Size = New System.Drawing.Size(192, 71)
-        Me.TabPage5.TabIndex = 2
-        Me.TabPage5.Text = "TabPage5"
-        Me.TabPage5.UseVisualStyleBackColor = True
+        Me.lblTrabajar.AutoSize = True
+        Me.lblTrabajar.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTrabajar.Location = New System.Drawing.Point(23, 14)
+        Me.lblTrabajar.Name = "lblTrabajar"
+        Me.lblTrabajar.Size = New System.Drawing.Size(109, 16)
+        Me.lblTrabajar.TabIndex = 1
+        Me.lblTrabajar.Text = "TURIKITAKATI"
         '
-        'TabPage6
+        'dgvMostrarComida
         '
-        Me.TabPage6.Location = New System.Drawing.Point(4, 25)
-        Me.TabPage6.Name = "TabPage6"
-        Me.TabPage6.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage6.Size = New System.Drawing.Size(192, 71)
-        Me.TabPage6.TabIndex = 3
-        Me.TabPage6.Text = "TabPage6"
-        Me.TabPage6.UseVisualStyleBackColor = True
+        Me.dgvMostrarComida.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgvMostrarComida.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvMostrarComida.Location = New System.Drawing.Point(3, 100)
+        Me.dgvMostrarComida.Name = "dgvMostrarComida"
+        Me.dgvMostrarComida.RowHeadersWidth = 51
+        Me.dgvMostrarComida.RowTemplate.Height = 24
+        Me.dgvMostrarComida.Size = New System.Drawing.Size(503, 377)
+        Me.dgvMostrarComida.TabIndex = 0
         '
-        'TabPage7
+        'Label4
         '
-        Me.TabPage7.Location = New System.Drawing.Point(4, 25)
-        Me.TabPage7.Name = "TabPage7"
-        Me.TabPage7.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage7.Size = New System.Drawing.Size(192, 71)
-        Me.TabPage7.TabIndex = 4
-        Me.TabPage7.Text = "TabPage7"
-        Me.TabPage7.UseVisualStyleBackColor = True
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(158, 442)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(44, 16)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Total: "
         '
-        'TabPage8
+        'txtPrecioTotal
         '
-        Me.TabPage8.Location = New System.Drawing.Point(4, 25)
-        Me.TabPage8.Name = "TabPage8"
-        Me.TabPage8.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage8.Size = New System.Drawing.Size(192, 71)
-        Me.TabPage8.TabIndex = 5
-        Me.TabPage8.Text = "TabPage8"
-        Me.TabPage8.UseVisualStyleBackColor = True
+        Me.txtPrecioTotal.Location = New System.Drawing.Point(208, 439)
+        Me.txtPrecioTotal.Name = "txtPrecioTotal"
+        Me.txtPrecioTotal.Size = New System.Drawing.Size(184, 22)
+        Me.txtPrecioTotal.TabIndex = 3
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(159, 51)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(173, 22)
+        Me.TextBox1.TabIndex = 5
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(61, 54)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(91, 16)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Buscar patillo:"
+        '
+        'Agregar
+        '
+        Me.Agregar.Image = CType(resources.GetObject("Agregar.Image"), System.Drawing.Image)
+        Me.Agregar.Location = New System.Drawing.Point(363, 37)
+        Me.Agregar.Name = "Agregar"
+        Me.Agregar.Size = New System.Drawing.Size(53, 50)
+        Me.Agregar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Agregar.TabIndex = 6
+        Me.Agregar.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(437, 37)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(53, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
         '
         'FrmPedidos
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1505, 529)
+        Me.Controls.Add(Me.btnFinalizar)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.Panel3)
         Me.Controls.Add(Me.Panel1)
         Me.Name = "FrmPedidos"
         Me.Text = "FrmPedidos"
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
-        Me.FlowLayoutPanel1.ResumeLayout(False)
-        Me.FlowLayoutPanel2.ResumeLayout(False)
-        Me.panelEntradas.ResumeLayout(False)
-        Me.TabControl1.ResumeLayout(False)
-        Me.TabControl2.ResumeLayout(False)
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        CType(Me.dgvPedidos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        CType(Me.dgvMostrarComida, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Agregar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -294,18 +309,18 @@ Partial Class FrmPedidos
     Friend WithEvents btnPostres As Button
     Friend WithEvents btnBebidas As Button
     Friend WithEvents btnPlatillos As Button
-    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
-    Friend WithEvents FlowLayoutPanel2 As FlowLayoutPanel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents dgvPedidos As DataGridView
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnFinalizar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents txtPrecioTotal As TextBox
+    Friend WithEvents Label4 As Label
     Friend WithEvents Panel2 As Panel
-    Friend WithEvents panelEntradas As Panel
-    Friend WithEvents TabControl2 As TabControl
-    Friend WithEvents TabPage3 As TabPage
-    Friend WithEvents TabPage4 As TabPage
-    Friend WithEvents TabPage5 As TabPage
-    Friend WithEvents TabPage6 As TabPage
-    Friend WithEvents TabPage7 As TabPage
-    Friend WithEvents TabPage8 As TabPage
-    Friend WithEvents TabControl1 As TabControl
-    Friend WithEvents TabPage1 As TabPage
-    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents lblTrabajar As Label
+    Friend WithEvents dgvMostrarComida As DataGridView
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Agregar As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- ProyectoDB/FrmPedidos.vb
+++ ProyectoDB/FrmPedidos.vb
@@ -6,4 +6,12 @@
     Private Sub btnEntradas_Click(sender As Object, e As EventArgs) Handles btnEntradas.Click
 
     End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles Agregar.Click
+
+    End Sub
+
+    Private Sub PictureBox1_Click_1(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
 End Class
